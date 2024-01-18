3e2bad48ee636a02cc31d7eaeee5edccf3f4834a
Wojtek-T-T <92437102+Wojtek-T-T@users.noreply.github.com>
2024-01-18 11:35:03 +0100

    Add files via upload

---
+++ kernel_modifications/unistd_mod.h
@@ -0,0 +1,29 @@
+#define __NR_RBSinitialize_rbs 451
+__SYSCALL(__NR_RBSinitialize_rbs, sys_RBSinitialize_rbs)
+
+#define __NR_RBSinitialize_task 452
+__SYSCALL(__NR_RBSinitialize_task, sys_RBSinitialize_task)
+
+#define __NR_RBSdata_transfer 453
+__SYSCALL(__NR_RBSdata_transfer, sys_RBSdata_transfer)
+
+#define __NR_RBSrelease_job 454
+__SYSCALL(__NR_RBSrelease_job, sys_RBSrelease_job)
+
+#define __NR_RBSwait_job 455
+__SYSCALL(__NR_RBSwait_job, sys_RBSwait_job)
+
+#define __NR_RBStry_execute 456
+__SYSCALL(__NR_RBStry_execute, sys_RBStry_execute)
+
+#define __NR_RBSnode_executed 457
+__SYSCALL(__NR_RBSnode_executed, sys_RBSnode_executed)
+
+#define __NR_RBSdelete_task 458
+__SYSCALL(__NR_RBSdelete_task, sys_RBSdelete_task)
+
+#define __NR_RBSspeed_test 459
+__SYSCALL(__NR_RBSspeed_test, sys_RBSspeed_test)
+
+#undef __NR_syscalls
+#define __NR_syscalls 460
